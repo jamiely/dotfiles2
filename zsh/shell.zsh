4651071ff7e7fd44f325c96dfe265cc0913e2efb
Jamie Ly <jamie@bricksimple.com>
2012-09-15 20:36:00 -0400

    Added various settings mostly related to powerline

--- zsh/shell.zsh
+++
@@ -1,8 +0,0 @@
-bindkey '^R' history-incremental-search-backward
-bindkey "^A" beginning-of-line
-bindkey "^E" end-of-line
-bindkey "^B" backward-word
-bindkey "^N" forward-word
-bindkey "^U" delete-line
-bindkey "^?" backward-delete-char
-
