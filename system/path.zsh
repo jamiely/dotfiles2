8d343af0c49ec054c9370e4d352dcf28ab5f96f3
Jamie Ly <me@jamie.ly>
2012-11-09 19:45:29 -0500

    Modified path so that it adds to path and does not overwrite

--- system/path.zsh
+++ system/path.zsh
@@ -1,4 +1,4 @@
-export PATH=".bin:$HOME/.rbenv/shims:$HOME/bin:/usr/local/bin:$ZSH/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin"
+export PATH=".bin:$HOME/.rbenv/shims:$HOME/bin:/usr/local/bin:$ZSH/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
