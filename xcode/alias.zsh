59476702ee9631aec96cebd8a3b2b10d960d7584
Jamie Ly <jly@empathylab.com>
2013-01-23 13:42:29 -0500

    Added xcode alias for symbolicating crashes

---
+++ xcode/alias.zsh
@@ -0,0 +1,2 @@
+alias symbolicatecrash="env DEVELOPER_DIR=\"/Applications/XCode.app/Contents/Developer\" /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/PrivateFrameworks/DTDeviceKit.framework/Versions/A/Resources/symbolicatecrash"
+
