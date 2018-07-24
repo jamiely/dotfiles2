4d5140cdca4d29a52039dae6da4bec8e027db7d3
Jamie Ly <jamie.ly@gmail.com>
2018-07-23 21:38:03 -0400

    Add a env var for java 8

--- java/vars.zsh
+++ java/vars.zsh
@@ -1,4 +1,5 @@
 export JAVA_HOME=$(/System/Library/Frameworks/JavaVM.framework/Versions/Current/Commands/java_home)
+export JAVA_HOME_8=/Library/Java/JavaVirtualMachines/jdk1.8.0_60.jdk/Contents/Home
 export JAVA_HOME_7=/Library/Java/JavaVirtualMachines/jdk1.7.0_79.jdk/Contents/Home/
 alias play7="env JAVA_HOME=$JAVA_HOME_7 play"
 
